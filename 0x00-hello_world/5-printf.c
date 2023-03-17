6b291863a8bdce890d4ec116a0c5fd1e62a23b96
TanjereeN98 <ahmadcyc35@gmail.com>
2023-03-16 17:54:59 -0700

    a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
 /**
- * main - a program that prints using printf function
- * Return value - 0 (success)
- */
-
+* main - a program that prints using printf function
+* Return: value - 0 (success)
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
-  * main - a program that prints the size of various types on the computer it is compiled and run on.
-  * return - 0 (Success)
-  */
+* main - Entry Point
+* Return: value 0
+*/
 int main(void)
-}
+{
 	char var_1;
 	int var_2;
 	long int var_3;
@@ -14,8 +14,7 @@ int main(void)
 	printf("Size of a char: %lu byte(s)\n", unsigned long sizeof(var_1));
 	printf("Size of an int: %lu byte(s)\n", unsigned long sizeof(var_2));
 	printf("Size of a long int: %lu byte(s)\n", unsigned long sizeof(var_3));
-	printf("Size of a long long int: %lu byte(s)\n",unsigned long sizeof(var_4));
-	printf("Size of a float: %lu byte(s)\n",unsigned long sizeof(var_5));
-	
+	printf("Size of a long long int: %lu byte(s)\n", unsigned long sizeof(var_4));
+	printf("Size of a float: %lu byte(s)\n", unsigned long sizeof(var_5));
 	return (0);
 }
