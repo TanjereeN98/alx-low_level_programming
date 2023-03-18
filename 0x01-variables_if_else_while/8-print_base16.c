dc0a0736966014ca6cbb40f8d75f724c651e2291
TanjereeN98 <ahmadcyc35@gmail.com>
2023-03-17 17:17:09 -0700

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	int a;
+	char c;
+
+	for (a = '0' ; a <= '9' ; a++)
+		putchar(a);
+
+	for (c = 'a' ; a <= 'f' ; c++)
+		putchar(c);
+
+	putchar('\n');
+	return (0);
+}
