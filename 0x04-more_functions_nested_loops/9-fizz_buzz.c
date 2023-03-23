3defab53c5aeaf0c6e2de964d70c434080d3e25b
TanjereeN98 <ahmadcyc35@gmail.com>
2023-03-23 15:47:00 -0700

    The Fizz-Buzz Test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,7 @@ int main(void)
 	
 	for (i = 1 ; i <= 100 ; i++)
 		if (i == 100)
-			printf("FizzBuzz\n");
+			printf("Buzz\n");
 		else if (i % 15 == 0)
 			printf("FizzBuzz ");
 		else if (i % 5 == 0)
