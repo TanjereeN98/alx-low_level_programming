5c7c23ba2074e385ca642bcfee3d317576c2b9bf
TanjereeN98 <ahmadcyc35@gmail.com>
2023-04-10 13:37:56 -0700

    a program that adds positive numbers.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-
+/**
+ * _isdigit - a function that checks for a digit (0 through 9)
+ * @c: input parameter.
+ * Return: 1 if digit or 0 if not.
+ */
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
