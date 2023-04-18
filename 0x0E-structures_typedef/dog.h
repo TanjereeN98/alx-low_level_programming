590ee25204b429c8640b92292f832b2f98e631d4
TanjereeN98 <ahmadcyc35@gmail.com>
2023-04-17 17:17:45 -0700

    a new type struct dog with the following elements:
    name, type = char *
    age, type = float
    owner, type = char *

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef POPPY
+#define POPPY
+/**
+  * struct dog - a structure for dogs
+  * @name: dog name
+  * @age: dog age
+  * @owner: dog owner name
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
