006928177db2e562ac885a74932a888aa9e3fdb8
TanjereeN98 <ahmadcyc35@gmail.com>
2023-05-04 17:20:36 -0700

    a function that prints the binary representation of a number.

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * binary_to_unit - function  that converts a binary number to an unsigned int
+ * @b: pointer to the string
+ * Return: 0 if it fails or integer if success
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int x = 0, y = 0, temp = 0;
+	unsigned int result = 0;
+
+	if (!b)
+		return (0);
+	while (b[y])
+	{
+		if (b[y] != '0' && b[y] != '1')
+			return (0);
+		y++;
+	}
+	while (b[x])
+	{
+		if (b[x] == '1')
+		{
+			temp = 1 << (y - x - 1);
+			result += temp;
+		}
+		x++;
+	}
+	return (result);
+}
---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ *  - function
+ *
+ *
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+
