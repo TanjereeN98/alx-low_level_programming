0ba14ac61599a8ae6676fd479395e6fd6ede8896
TanjereeN98 <ahmadcyc35@gmail.com>
2023-06-16 02:12:08 +0300

    &message

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -8,11 +8,11 @@ size_t dlistint_len(const dlistint_t *h)
 {
 	size_t len = 0;
 	const dlistint_t *current = h;
-	
+
 	while (current)
 	{
 		len++;
 		current = current->next;
 	}
-	return len;
+	return (len);
 }
---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * add_dnodeint - adds a new node at the beginning of a dlistint_t
+ * @head: pointer to the head of the list
+ * @n: data of the new node
+ * Return: pointer to the new node
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+
