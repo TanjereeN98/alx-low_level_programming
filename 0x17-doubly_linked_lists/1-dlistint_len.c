91546e4e9bdc2477ce09731505ae91d7b570ccb9
TanjereeN98 <ahmadcyc35@gmail.com>
2023-06-16 02:10:02 +0300

    &message

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * dlistint_len - print the len of the list
+ * @h: head of the list
+ * Return: len of the list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t len = 0;
+	const dlistint_t *current = h;
+	
+	while (current)
+	{
+		len++;
+		current = current->next;
+	}
+	return len;
+}
