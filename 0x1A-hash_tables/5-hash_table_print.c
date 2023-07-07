c05f23484b2d4d524870fc5935e8dd95e9af3e13
TanjereeN98 <ahmadcyc35@gmail.com>
2023-07-07 12:48:47 +0300

    Qol Edit on Task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -15,17 +15,14 @@ void hash_table_print(const hash_table_t *ht)
 	printf("{");
 	for (i = 0 ; i < ht->size ; i++)
 	{
-		if (ht->array[i])
+		curr_node = ht->array[i];
+		while (curr_node)
 		{
-			curr_node = ht->array[i];
-			while (curr_node)
-			{
-				if (is_new)
-					printf(", ");
-				printf("'%s': '%s'", curr_node->key, curr_node->value);
-				is_new = 1;
-				curr_node = curr_node->next;
-			}
+			if (is_new)
+				printf(", ");
+			printf("'%s': '%s'", curr_node->key, curr_node->value);
+			is_new = 1;
+			curr_node = curr_node->next;
 		}
 	}
 	printf("}\n");
