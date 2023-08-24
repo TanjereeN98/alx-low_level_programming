b0b318ad5db46deae25b301efe0e88823ca6587d
TanjereeN98 <ahmadcyc35@gmail.com>
2023-08-24 03:45:05 +0300

    Last fix for last task

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,19 +1,35 @@
 #!/usr/bin/python3
-"""returns the perimeter of the island described in grid"""
+"""
+Island perimeter Task
+"""
 
 
 def island_perimeter(grid):
-    """returns the perimeter of the island described in grid"""
-    per = 0
+    """
+    Returns the perimeter of the island described in the grid.
+    """
+    perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] == 1:
-                if grid[i - 1][j] == 0 or not grid[i - 1][j]:
-                    per += 1
-                if grid[i][j + 1] == 0 or not grid[i][j + 1]:
-                    per += 1
-                if grid[i + 1][j] == 0 or not grid[i + 1][j]:
-                    per += 1
-                if grid[i][j - 1] == 0 or not grid[i][j - 1]:
-                    per += 1
-    return per
+                try:
+                    if grid[i - 1][j] == 0:
+                        perimeter += 1
+                except IndexError:
+                    perimeter += 1
+                try:
+                    if grid[i + 1][j] == 0:
+                        perimeter += 1
+                except IndexError:
+                    perimeter += 1
+                try:
+                    if grid[i][j - 1] == 0:
+                        perimeter += 1
+                except IndexError:
+                    perimeter += 1
+                try:
+                    if grid[i][j + 1] == 0:
+                        perimeter += 1
+                except IndexError:
+                    perimeter += 1
+    return perimeter
