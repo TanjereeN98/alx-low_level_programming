8f2e449b4bbfdb597438a568a3b7dce573da725f
TanjereeN98 <ahmadcyc35@gmail.com>
2023-08-24 03:43:17 +0300

    Another fix for last task

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,35 +1,19 @@
 #!/usr/bin/python3
-"""
-Island perimeter
-"""
+"""returns the perimeter of the island described in grid"""
 
 
 def island_perimeter(grid):
-    """
-    Returns the perimeter of the island described in grid.
-    """
-    perimeter = 0
-    for row in range(len(grid)):
-        for col in range(len(grid[0])):
-            if grid[row][col] == 1:
-                try:
-                    if grid[row - 1][col] == 0:
-                        perimeter += 1
-                except IndexError:
-                    perimeter += 1
-                try:
-                    if grid[row + 1][col] == 0:
-                        perimeter += 1
-                except IndexError:
-                    perimeter += 1
-                try:
-                    if grid[row][col - 1] == 0:
-                        perimeter += 1
-                except IndexError:
-                    perimeter += 1
-                try:
-                    if grid[row][col + 1] == 0:
-                        perimeter += 1
-                except IndexError:
-                    perimeter += 1
-    return perimeter
+    """returns the perimeter of the island described in grid"""
+    per = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                if grid[i - 1][j] == 0 or not grid[i - 1][j]:
+                    per += 1
+                if grid[i][j + 1] == 0 or not grid[i][j + 1]:
+                    per += 1
+                if grid[i + 1][j] == 0 or not grid[i + 1][j]:
+                    per += 1
+                if grid[i][j - 1] == 0 or not grid[i][j - 1]:
+                    per += 1
+    return per
